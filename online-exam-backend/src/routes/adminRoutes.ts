cd3c983c887e6d592b4483525d491596ce53ed1b
thearsalan1 <arsalanloq@gmail.com>
2026-06-23 11:43:12 +0530

    feat: add results module with student and admin APIs, including result retrieval and dashboard stats

--- online-exam-backend/src/routes/adminRoutes.ts
+++ online-exam-backend/src/routes/adminRoutes.ts
@@ -3,13 +3,53 @@ import {
   deleteStudent,
   getAllStudents,
   getStudentById,
-} from "../controllers/AdminController";
+} from "../controllers/adminController";
 import auth from "../middleware/auth.middleware";
+import {
+  getAllResults,
+  getDashboardStats,
+  getExamResults,
+  getResultDetailAdmin,
+  getStudentResultsAdmin,
+} from "../controllers/resultController";
 
 const router = Router();
 
 router.get("/", auth.authenticate, auth.adminMiddleware, getAllStudents);
 router.get("/:id", auth.authenticate, auth.adminMiddleware, getStudentById);
 router.delete("/:id", auth.authenticate, auth.adminMiddleware, deleteStudent);
+router.get("/results", auth.authenticate, auth.adminMiddleware, getAllResults);
+
+// GET /api/admin/results/:attemptId
+router.get(
+  "/results/:attemptId",
+  auth.authenticate,
+  auth.adminMiddleware,
+  getResultDetailAdmin,
+);
+
+// GET /api/admin/exams/:examId/results
+router.get(
+  "/exams/:examId/results",
+  auth.authenticate,
+  auth.adminMiddleware,
+  getExamResults,
+);
+
+// GET /api/admin/student/:studentId/results
+router.get(
+  "/student/:studentId/results",
+  auth.authenticate,
+  auth.adminMiddleware,
+  getStudentResultsAdmin,
+);
+
+// GET /api/admin/dashboard/stats
+router.get(
+  "/dashboard/stats",
+  auth.authenticate,
+  auth.adminMiddleware,
+  getDashboardStats,
+);
 
 export default router;
